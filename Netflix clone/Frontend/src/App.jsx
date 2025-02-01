aa507c424229aaaebe055c44e971233296ccc2c3
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-02-01 13:37:22 +0545

    wip:- signup page created

--- Netflix clone/Frontend/src/App.jsx
+++ Netflix clone/Frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Homepage from './pages/Homepage'
 
 const App = () => {
   return (
-    <div className='font-bold text-center bg-pink-300'>hi</div>
+    <Routes>
+      <Route path='/login' element={<Login/>} />
+      <Route path='/signup' element={<Signup/>} />
+      <Route path='/' element={<Homepage/>} />
+    </Routes>
   )
 }
-
 export default App
---
+++ Netflix clone/Frontend/src/pages/Homepage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div className='home-bg min-h-screen'></div>
+  )
+}
+
+export default Homepage
